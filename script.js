0a0c6bd4f48688aa60869b8e9c102234df7d7202
Thiago <161526886+Humano123@users.noreply.github.com>
2024-06-18 16:45:32 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ if (isMobile()) {
     overlay.className = 'mobile-overlay';
     overlay.innerHTML = `
         <h1>Ops</h1>
-        <p>Detectamos uma falha!</p>
+        <p>Retiramos a entrada de dispositivos moveis(Celular/Tablet), infelizmente retiramos essa entrada por conta de BUGS que é bem dificil de arrumar, então resolvemos retirar esta entrada, além do mais para ajustar o tamanho da tela, é muito complicado, pois teremos que ajustar para CADA tela</p>
     `;
     document.body.appendChild(overlay);
 }
